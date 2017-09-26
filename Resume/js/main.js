80573c736b12c28a458856adaa1fe9b72ec002e2
ndessi <dessi.netsanet@gmail.com>
2017-09-26 09:35:20 -0700

    Add files via upload

---
+++ Resume/js/main.js
@@ -0,0 +1,11 @@
+// Make it rain!
+
+$(window).resize(function() {
+
+  if ($(window).width() > 928) {
+    $('aside').height($('section').height())
+  } else {
+    $('aside').height('auto')
+  }
+
+});
